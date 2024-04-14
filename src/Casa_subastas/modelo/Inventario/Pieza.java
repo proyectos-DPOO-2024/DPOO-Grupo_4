0e6f732b57b7da0cbcb746318e56352a93716790
Juan Pablo Castro <j.idarragam@uniandes.edu.co>
2024-04-14 17:18:32 -0500

    Pieza

--- src/Casa_subastas/modelo/Inventario/Pieza.java
+++ src/Casa_subastas/modelo/Inventario/Pieza.java
@@ -5,23 +5,23 @@ import javax.swing.Spring;
 public abstract class Pieza {
 
 	private String nombrePieza;
-	private int Precio;
+	private int precio;
 	private String nombrePropietario;
 	private int diasConsignacion;
 	private Boolean paraVentafijo;
-	private Boolean Bloqueada;
-	private Boolean Comprada;
+	private Boolean bloqueada;
+	private Boolean comprada;
 	
 	public Pieza (String nombrePieza, int Precio, 
 			String nombrePropietario, int diasConsignacion, 
 			Boolean paraVentafijo, Boolean Bloqueada, Boolean Comprada) {
 		this.nombrePieza = nombrePieza;
-		this.Precio = Precio;
+		this.precio = Precio;
 		this.nombrePropietario = nombrePropietario;
 		this.diasConsignacion = diasConsignacion;
 		this.paraVentafijo = paraVentafijo;
-		this.Bloqueada = Bloqueada;
-		this.Comprada = Comprada;
+		this.bloqueada = Bloqueada;
+		this.comprada = Comprada;
 	}
 	public String getNombrepieza() {
 		return this.nombrePieza;
@@ -29,13 +29,23 @@ public abstract class Pieza {
 	public Cliente getPropietario() {
 		return null;
 	}
+	public boolean getParaVentaValorFijo() {
+		return this.paraVentafijo;
+	}
+	public boolean getBloqueada() {
+		return this.bloqueada;
+	}
 	public Cliente cambiarPropietario(Cliente nuevoPropietario) {
 		return null;
 	}
 	public void Comprado() {
-		this.Comprada = true;
+		this.comprada = true;
 	}
-	public void Bloqueado() {
-		this.Bloqueada = true;
+	public void Bloquear() {
+		this.bloqueada = true;
 	}
+	public void desbloquear() {
+		this.bloqueada = false;
+	}
+	public abstract int getCosto();
 }
