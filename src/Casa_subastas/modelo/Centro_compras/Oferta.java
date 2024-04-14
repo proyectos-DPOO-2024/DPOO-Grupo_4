43a83c4adb7109bf9ee87e9651a651c61beaa5f0
Juan Pablo Castro <j.idarragam@uniandes.edu.co>
2024-04-14 11:28:14 -0500

    Oferta

---
+++ src/Casa_subastas/modelo/Centro_compras/Oferta.java
@@ -0,0 +1,60 @@
+package Casa_subastas.modelo.Centro_compras;
+
+import Casa_subastas.modelo.Inventario.Pieza;
+import Casa_subastas.modelo.usuarios.Cliente;
+
+public class Oferta {
+
+	///atributos\\\
+	private Pieza pieza;
+	
+	private Cliente Comprador;
+	
+	private int precio;
+	
+	private boolean ofertaVerificada;
+	
+	///metodos\\\
+	
+	//constructor\\
+
+	public Oferta(Pieza pieza, int precio, Cliente comprador, boolean ofertaVerificada) {
+		super();
+		this.pieza = pieza;
+		this.precio = precio;
+		this.Comprador = comprador;
+		this.ofertaVerificada = false;
+	}
+	
+	public Oferta(Pieza pieza, Cliente comprador, boolean ofertaVerificada) {
+		super();
+		this.pieza = pieza;
+		this.precio = pieza.getCosto();
+		this.Comprador = comprador;
+		this.ofertaVerificada = false;
+	}
+	
+	///Metodos importantes\\\
+	
+	public void confirmarOferta() {
+		ofertaVerificada = true;
+	}
+
+	public Pieza getPieza() {
+		return pieza;
+	}
+
+	public Cliente getComprador() {
+		return Comprador;
+	}
+	
+	public boolean esConfirmada()
+	{
+		return ofertaVerificada;
+	}
+	
+	
+	
+	
+	
+}
