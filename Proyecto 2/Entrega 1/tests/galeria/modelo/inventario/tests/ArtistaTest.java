d726d661191c9878a64abff08cbc04b3d2f1ec13
Juan Pablo Castro <j.idarragam@uniandes.edu.co>
2024-05-01 17:28:43 -0500

    primer test

--- Proyecto 2/Entrega 1/tests/galeria/modelo/inventario/tests/ArtistaTest.java
+++ Proyecto 2/Entrega 1/tests/galeria/modelo/inventario/tests/ArtistaTest.java
@@ -2,6 +2,7 @@ package galeria.modelo.inventario.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,13 +13,19 @@ public class ArtistaTest
 	private Artista artista;
 
 	@BeforeEach
-	void setUp() throws Exception {
+	void setUp() {
 		artista = new Artista("nombreArtista");
 	}
+    @AfterEach
+    void tearDown( )
+    {
+    }
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    void testArtista( )
+    {
+        assertEquals( "nombreArtista", artista.getNombre( ), "El nombre del artista no es el correcto" );
+        assertEquals( 0, artista.getNombrePiezas().size(), "El nuevo artista no debería tener piezas" );
+    }
 
 }
